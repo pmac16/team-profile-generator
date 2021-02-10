7b72d9a025524e0eb4348a5a2aec9d9bb4a66a1a
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-02-10 15:50:16 -0800

    logic for engineer.js

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,11 @@
+const { TestScheduler } = require('jest');
+const Employee = require('../lib/Employee.js');
+
+jest.mock('../lib/Employee.js');
+
+test('has an office number', () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.officeNumber).toEqual(expect.any(Number));
+});
+
